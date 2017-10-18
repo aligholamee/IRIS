7c732bfee05bb1d79791f7f8fdf3e15ac701cc03
Ali Gholami <aligholami7596@gmail.com>
2017-10-18 15:42:39 +0330

    Libraries were added

--- sources/Classifier.py
+++ sources/Classifier.py
@@ -1,19 +1,26 @@
-# Python version
-import sys
-print('Python: {}'.format(sys.version))
-# scipy
-import scipy
-print('scipy: {}'.format(scipy.__version__))
-# numpy
-import numpy
-print('numpy: {}'.format(numpy.__version__))
-# matplotlib
-import matplotlib
-print('matplotlib: {}'.format(matplotlib.__version__))
-# pandas
+# IRIS Classification 
+# ========================================
+# [] File Name : Classifier.py
+#
+# [] Creation Date : October 2017
+#
+# [] Created By : Ali Gholami (aligholami7596@gmail.com)
+# ========================================
+#
+
+# Load libraries
 import pandas
-print('pandas: {}'.format(pandas.__version__))
-# scikit-learn
-import sklearn
-print('sklearn: {}'.format(sklearn.__version__))
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
 
