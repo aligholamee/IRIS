8b06a66e569299dd3178e1fd423bd87d7cd9bce7
Ali Gholami <aligholami7596@gmail.com>
2017-10-18 15:57:00 +0330

    Fix dataset problems

--- sources/Classifier.py
+++ sources/Classifier.py
@@ -24,3 +24,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+
+# Initialise the dataset
+dataset = pandas.read_csv(url, names=names)
